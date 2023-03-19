ae06903cab62bbb7567df5a6360abb823a8ec908
窦超 <2967601670@qq.com>
2023-03-19 14:49:01 +0800

    day0318 第四次提交 master

--- src/main/java/com/atguigu/test/Demo.java
+++ src/main/java/com/atguigu/test/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println(" I Love You ! ");
         System.out.println(" atguigu ");
+        System.out.println(" forever ");
     }
 
 }
